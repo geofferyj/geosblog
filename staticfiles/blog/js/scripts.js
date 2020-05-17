dec50f5b174d8b73c9b95ecd8f7435c88dcdda12
geofferyj <geofferyjoseph1@gmail.com>
2020-05-17 07:46:49 +0100

    collect static

---
+++ staticfiles/blog/js/scripts.js
@@ -0,0 +1,107 @@
+/*
+   
+    Template Name : DevBlog - Personal Blog Template
+    Author : UiPasta Team
+    Website : https://www.uipasta.com/
+    Support : https://www.uipasta.com/contact-us/
+	
+	
+*/
+
+
+
+/*
+   
+   Table Of Content
+   
+   1. Preloader
+   2. Smooth Scroll
+   3. Scroll To Top
+   4. Tooltip
+   5. Popover
+   6. Ajaxchimp for Subscribe Form
+   7. Video and Google Map Popup
+   8. Magnific Popup
+   9. Image Carousel/Slider
+  10. Load More Post
+  11. Load More Portfolio
+  12. End Box (Popup When Scroll Down)
+ 
+
+*/
+
+
+(function ($) {
+    'use strict';
+
+    jQuery(document).ready(function () {
+
+        
+       /* Preloader */
+		
+        $(window).load(function () {
+            $('.preloader').delay(100).fadeOut('slow');
+         });
+		 		
+		
+		
+       /* Smooth Scroll */
+
+        $('a.smoth-scroll').on("click", function (e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 50
+            }, 1000);
+            e.preventDefault();
+        });
+				 
+		
+		
+		
+       /* Scroll To Top */
+		
+        $(window).scroll(function(){
+        if ($(this).scrollTop() >= 500) {
+            $('.scroll-to-top').fadeIn();
+         } else {
+            $('.scroll-to-top').fadeOut();
+         }
+         });
+	
+	
+        $('.scroll-to-top').click(function(){
+          $('html, body').animate({scrollTop : 0},800);
+          return false;
+          });
+		  
+		  
+		  
+       /* Tooltip */
+	   
+        $('[data-toggle="tooltip"]').tooltip();
+
+
+
+       /* Popover */
+	   
+        $('[data-toggle="popover"]').popover();		  
+		  
+		  
+       /* Load More Post */	
+	   	
+        $("div.blog-post").slice(0, 4).show();
+          $("#load-more-post").on('click', function (e) {
+             e.preventDefault();
+             $("div.blog-post:hidden").slice(0, 1).slideDown(300);
+             if ($("div.blog-post:hidden").length == 0) {
+             $('#post-end-message').html('<div class="end">End</div>').fadeIn(800);
+             $("#load-more-post").fadeOut(100);
+              }
+             });
+			 
+
+		
+		   
+            });
+
+   })(jQuery);
