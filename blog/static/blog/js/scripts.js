fc7ff742d049c667f7993c057e9b24fd5d54747f
geofferyj <geofferyjoseph1@gmail.com>
2020-05-18 01:50:09 +0100

    fixed comments

--- blog/static/blog/js/scripts.js
+++ blog/static/blog/js/scripts.js
@@ -1,107 +1,75 @@
-/*
-   
-    Template Name : DevBlog - Personal Blog Template
-    Author : UiPasta Team
-    Website : https://www.uipasta.com/
-    Support : https://www.uipasta.com/contact-us/
-	
-	
-*/
-
-
-
-/*
-   
-   Table Of Content
-   
-   1. Preloader
-   2. Smooth Scroll
-   3. Scroll To Top
-   4. Tooltip
-   5. Popover
-   6. Ajaxchimp for Subscribe Form
-   7. Video and Google Map Popup
-   8. Magnific Popup
-   9. Image Carousel/Slider
-  10. Load More Post
-  11. Load More Portfolio
-  12. End Box (Popup When Scroll Down)
- 
-
-*/
-
-
-(function ($) {
-    'use strict';
-
-    jQuery(document).ready(function () {
-
-        
-       /* Preloader */
-		
-        $(window).load(function () {
-            $('.preloader').delay(100).fadeOut('slow');
-         });
-		 		
-		
-		
-       /* Smooth Scroll */
-
-        $('a.smoth-scroll').on("click", function (e) {
-            var anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $(anchor.attr('href')).offset().top - 50
-            }, 1000);
-            e.preventDefault();
-        });
-				 
-		
-		
-		
-       /* Scroll To Top */
-		
-        $(window).scroll(function(){
+$(function () {
+    /* Preloader */
+    $(window).on('load', function () {
+        $('pre').addClass('ss-container');
+        $('.preloader').delay(100).fadeOut('slow');
+    });
+    /* Smooth Scroll */
+    $('a.smoth-scroll').on("click", function (e) {
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top - 50
+        }, 1000);
+        e.preventDefault();
+    });
+    /* Scroll To Top */
+    $(window).on('scroll', function () {
         if ($(this).scrollTop() >= 500) {
             $('.scroll-to-top').fadeIn();
-         } else {
+        } else {
             $('.scroll-to-top').fadeOut();
-         }
-         });
-	
-	
-        $('.scroll-to-top').click(function(){
-          $('html, body').animate({scrollTop : 0},800);
-          return false;
-          });
-		  
-		  
-		  
-       /* Tooltip */
-	   
-        $('[data-toggle="tooltip"]').tooltip();
+        }
+    });
+    $('.scroll-to-top').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+
+    });
+});
 
 
+var infinite = new Waypoint.Infinite({
+    element: $('.infinite-container')[0],
+    handler: function (direction) {},
+    offset: 'bottom-in-view',
+    onBeforePageLoad: function () {
+        $('.preloader').show();
+    },
+    onAfterPageLoad: function () {
+        $('.preloader').hide();
+    }
+});
 
-       /* Popover */
-	   
-        $('[data-toggle="popover"]').popover();		  
-		  
-		  
-       /* Load More Post */	
-	   	
-        $("div.blog-post").slice(0, 4).show();
-          $("#load-more-post").on('click', function (e) {
-             e.preventDefault();
-             $("div.blog-post:hidden").slice(0, 1).slideDown(300);
-             if ($("div.blog-post:hidden").length == 0) {
-             $('#post-end-message').html('<div class="end">End</div>').fadeIn(800);
-             $("#load-more-post").fadeOut(100);
-              }
-             });
-			 
+document.addEventListener("DOMContentLoaded", () => {
 
-		
-		   
-            });
+    document.querySelectorAll('pre').forEach((block) => {
+        hljs.highlightBlock(block);
+    });
 
-   })(jQuery);
+    document.querySelectorAll('code').forEach((block) => {
+        hljs.highlightBlock(block);
+    });
+
+    renderMathInElement(document.body, {
+        delimiters: [
+          { left: "$$", right: "$$", display: true },
+          { left: "\\[", right: "\\]", display: true },
+          { left: "$", right: "$", display: false },
+          { left: "\\(", right: "\\)", display: false }
+        ]
+      });
+});
+
+
+function displayToggle(id) {
+    let x = document.getElementById(id);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
--- geoffery/settings.py
+++ geoffery/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f$H%dc|E:%,^6c-%KiS<($KD,$GT!jY+!`aA8*Qx05{j}vjdskh&^%&888gfg,jdJ'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'),]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
